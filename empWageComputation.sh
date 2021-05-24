7c1dbe0e24a20d53408a12937f15db15f332d0cb
Subendudas <subendudas08@gmail.com>
2021-05-24 18:17:42 +0530

    Added Employee is present or Not

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 echo "Welcome to Shell Script"
+ isEmployeePresent=$(( RANDOM%2 ))
+if [ $isEmployeePresent -eq 1 ]
+then 
+echo "Employee is Present"
+else
+echo "Employee is Absent"
+fi
